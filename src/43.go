7c2f751a3c1bc5cdd7929e7ae64abecf7840479b
yenithnoticias <yenithnoticias@gmail.com>
2025-05-27 14:15:56 +0000

    Add 43.go

---
+++ src/43.go
@@ -0,0 +1,13 @@
+using System.Security.Cryptography;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Numerics;
+using System;
+
+class Solution {
+    static void Main() {
+        Console.WriteLine("Hello, World!");
+    }
+}
